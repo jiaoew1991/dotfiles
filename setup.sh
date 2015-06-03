490d5ff75349e0c4b168adba77f966a94a98ef0f
Enwei Jiao <jiaoew2011@gmail.com>
2015-06-03 13:29:22 +0800

    fix setup bug

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 current=`pwd`
 ln -s $current/.vimrc ~/.vimrc
 ln -s $current/.editorconfig ~/.editorconfig
+mkdir ~/.vim/bundle
+ln -s $current/vundle ~/.vim/bundle/Vundle.vim
 
 vim -E -c PluginInstall -c qall!
 ~/.vim/bundle/YouCompleteMe/install.sh --clang-completer --system-libclang
-cd $current/bundle/tern_for_vim
+cd ~/.vim/bundle/tern_for_vim
 npm install
