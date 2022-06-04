46e443d0200046b693c4083903d1c19e8a8d0cbb
Enwei Jiao <jiaoew2011@gmail.com>
2022-06-04 11:29:20 +0800

    add zshrc, rum vundle

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 current=`pwd`
-ln -s $current/.vimrc ~/.vimrc
-ln -s $current/.editorconfig ~/.editorconfig
-mkdir ~/.vim/bundle
-ln -s $current/vundle ~/.vim/bundle/Vundle.vim
+ln -s $current/vimrc ~/.vimrc
+ln -s $current/nvimrc ~/.nvimrc
+ln -s $current/zshrc ~/.zshrc
 
-vim -E -c PluginInstall -c qall!
-~/.vim/bundle/YouCompleteMe/install.sh --clang-completer --system-libclang
-cd ~/.vim/bundle/tern_for_vim
-npm install
