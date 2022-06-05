1d1beb37c21510e2b86266e2f105945ed44e105a
Enwei Jiao <jiaoew2011@gmail.com>
2022-06-05 19:15:00 +0800

    lvim config

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ ln -s $current/vimrc ~/.vimrc
 ln -s $current/nvimrc ~/.nvimrc
 ln -s $current/zshrc ~/.zshrc
 ln -s $current/editorconfig ~/.editorconfig
+ln -s $current/lvim.lua ~/.config/lvim/config.lua
 
 if [[ ! -f ~/.vim/autoload/plug.vim ]]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
